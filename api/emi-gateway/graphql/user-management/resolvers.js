149033b6c626154ab7214a9162f8eb355ffce551
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-01-24 13:40:07 -0500

    Removes logs

--- api/emi-gateway/graphql/user-management/resolvers.js
+++ api/emi-gateway/graphql/user-management/resolvers.js
@@ -67,7 +67,6 @@ module.exports = {
   //// QUERY /////
   Query: {
     getUsers(root, args, context) {
-      console.log("getUsers(root, args, context) ==> ", args);
       return RoleValidator.checkPermissions$(
         context.authToken.realm_access.roles,
         contextName,
